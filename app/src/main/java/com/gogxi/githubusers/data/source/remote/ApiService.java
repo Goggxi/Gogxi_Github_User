f02a82e87211cae8de4cac784c8bb217a8ba4aea
gogxi <goggxi@gmail.com>
2020-04-23 23:35:41 +0800

    edit token

--- app/src/main/java/com/gogxi/githubusers/data/source/remote/ApiService.java
+++ app/src/main/java/com/gogxi/githubusers/data/source/remote/ApiService.java
@@ -15,18 +15,18 @@ import retrofit2.http.Query;
 public interface ApiService {
 
     @GET("/search/users")
-    @Headers("Authentication: token <4bbba7ffce1b5819d4f45d92def0799a725f9301>")
+    @Headers("Authentication: token <82aea1bf19e94391682eb2067a21824cf81dfe08>")
     Call<UsersResponse> getUser(@Query("q") String username);
 
     @GET("/users/{username}")
-    @Headers("Authentication: token <4bbba7ffce1b5819d4f45d92def0799a725f9301>")
+    @Headers("Authentication: token <82aea1bf19e94391682eb2067a21824cf81dfe08>")
     Call<UsersDetail> getDetailUser(@Path("username") String username);
 
     @GET("/users/{username}/followers")
-    @Headers("Authentication: token <4bbba7ffce1b5819d4f45d92def0799a725f9301>")
+    @Headers("Authentication: token <82aea1bf19e94391682eb2067a21824cf81dfe08>")
     Call<List<Users>> getFollowersUser(@Path("username") String username);
 
     @GET("/users/{username}/following")
-    @Headers("Authentication: token <4bbba7ffce1b5819d4f45d92def0799a725f9301>")
+    @Headers("Authentication: token <82aea1bf19e94391682eb2067a21824cf81dfe08>")
     Call<List<Users>> getFollowingUser(@Path("username") String username);
 }
